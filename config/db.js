3dc5283ca272541c6451dc8219213ccaca7b83c7
=Nareshapp856 <virtuallab@nareshit.com>
2025-04-02 14:39:33 +0530

    ashish commit

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 
 // MySQL connection configuration
 const connection = mysql.createConnection({
-  host: "49.207.10.13", // ✅ MySQL Host
-  port: 3306, // ✅ MySQL Default Port, // Change to your MySQL server address
-  user: "sa", // Change to your MySQL username
-  password: "Password@123", // Change to your MySQL password
-  database: "practicedb",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 // Establish the MySQL connection
